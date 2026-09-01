e4af2a5c7022cacb365c826214320d6ca5d9ae4b
Dhika Putra <dhikwrr@gmail.com>
2026-09-01 23:17:21 +0700

    pertemuan-01: add new fitur-sapaan branch

--- pertemuan-01/main.go
+++ pertemuan-01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 )
 
 // TODO(Level 4): ganti dengan NRP kalian sendiri, contoh: "5025201012"
@@ -23,7 +24,13 @@ func Sapa(nama string) string {
 // TODO(Level 5): gabungkan Nama, NRP, dan hasil runtime.Version() jadi satu
 // string siap cetak (lihat contoh format di SOAL.md).
 func CetakInfo(nama string) string {
-	return "TODO"
+	return fmt.Sprintf(
+		"Nama: %s\nNRP: %s \n%s %s",
+		nama,
+		NRP,
+		runtime.Version(),
+		Sapa(nama),
+	)
 }
 
 func main() {
