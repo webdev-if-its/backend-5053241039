3e5dff2abf3584deb95675a99033a13a1fb27e6d
Dhika <dhikaaptw@gmail.com>
2026-09-15 17:40:56 +0700

    pertemuan-03: level 7 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -74,7 +74,21 @@ func HapusTugas(toko *TokoTugas, id int) error {
 // HapusTugasTercatat memanggil HapusTugas, lalu memakai defer untuk
 // MENCATAT hasilnya ke toko.Log -- baik saat berhasil maupun saat gagal.
 func HapusTugasTercatat(toko *TokoTugas, id int) error {
-	panic("belum diimplementasikan")
+	var err error
+
+	defer func() {
+		if err == nil {
+			catatan := fmt.Sprintf("hapus id=%d: berhasil", id)
+			toko.Log = append(toko.Log, catatan)
+		} else {
+			catatan := fmt.Sprintf("hapus id=%d: gagal", id)
+			toko.Log = append(toko.Log, catatan)
+		}
+	}()
+
+	err = HapusTugas(toko, id)
+
+	return err
 }
 
 // AmankanPanggilan menjalankan fn. Kalau fn panic, AmankanPanggilan
